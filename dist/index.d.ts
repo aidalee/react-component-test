df9a791db81f4cd89e981b2e20ae18ad8f4035be
lx <lixue@souche.com>
2020-12-30 18:34:41 +0800

    feat: 打包发布

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,7 @@
 import './styles/index.scss';
+export { default as Button } from './components/Button';
+export { default as Alert } from './components/Alert/alert';
+export { default as Icon } from './components/Icon';
+export { default as Menu } from './components/Menu';
+export { default as MenuItem } from './components/Menu/menuItem';
+export { default as SubMenu } from './components/Menu/subMenu';
